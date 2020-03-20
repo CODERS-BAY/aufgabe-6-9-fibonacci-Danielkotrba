e2011521dc88f3ce6ab42d9c9c4c1426293a3a35
Daniel <Daniel.kotrba@gmx.at>
2020-03-20 13:44:40 +0100

    Fibonacci Update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Enter a number:");
         Scanner scanner = new Scanner(System.in);
         Integer n = scanner.nextInt();
         System.out.println(getFibonacci(n));
     }
-
     public static Integer getFibonacci(Integer n) {
         if (n == 0){
             return 0;
@@ -16,6 +13,5 @@ public class Main {
             return 1;
         }
         return getFibonacci(n-1) + getFibonacci(n-2);
-
     }
 }
